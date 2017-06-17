95a9032e9f871d3f293fe09c9b7c4712d6bcbb89
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-06-18 01:04:49 +1000

    Improved performance. Fixed issue where push_back was used instead of at.

--- neural/Neuron.cpp
+++ neural/Neuron.cpp
@@ -1,64 +1,60 @@
 #include "Neuron.h"
 #include "MathHelper.h"
 namespace Neural {
-	NeuronC::NeuronC(vector<double>& weights) {
+	NeuronC::NeuronC(shared_ptr<vector<double>> weights) {
 		this->weights = weights;
 	}
 
 	void NeuronC::AddWeight(double weight = 0) {
-		this->weights.push_back(weight);
+		this->weights->push_back(weight);
 	}
 
 	void NeuronC::RemoveNeuronWeight(size_t neuronIndex) {
 		size_t weightIndex = neuronIndex + 1;
-		this->weights.erase(this->weights.begin() + weightIndex);
+		this->weights->erase(this->weights->begin() + weightIndex);
 	}
 
-	void NeuronC::SetWeights(vector<double>& weights) {
+	void NeuronC::SetWeights(shared_ptr<vector<double>> weights) {
 		this->weights = weights;
 	}
 
 	double NeuronC::GetWeight(size_t weightIndex) {
-		return this->weights[weightIndex];
+		return this->weights->at(weightIndex);
 	}
 
 	size_t NeuronC::GetWeightSize() {
-		return this->weights.size();
+		return this->weights->size();
 	}
 
 	double NeuronC::GetNeuronWeight(size_t weightIndex) {
-		return this->weights[weightIndex + 1];
+		return this->weights->at(weightIndex + 1);
 	}
 
 	double NeuronC::GetThreshold() {
-		return this->weights[0];
+		return this->weights->at(0);
 	}
 
 	void NeuronC::SetWeight(size_t weightIndex, double weight) {
-		this->weights[weightIndex] = weight;
+		this->weights->at(weightIndex) = weight;
 	}
 
 	void NeuronC::SetThresholdWeight(double weight) {
-		this->weights[0] = weight;
+		this->weights->at(0) = weight;
 	}
 
 	void NeuronC::SetNeuronWeight(size_t neuronIndex, double weight) {
-		this->weights[neuronIndex + 1] = weight;
+		this->weights->at(neuronIndex + 1) = weight;
 	}
 
-	double NeuronC::GetOutput(vector<double>& inputs) {
-		double output = -this->weights[0];
+	double NeuronC::GetOutput(const vector<double>& inputs) {
+		double output = -this->weights->at(0);
 		for (size_t i = 0; i < inputs.size(); i++) {
-			output += this->weights[i + 1] * inputs[i];
+			output += this->weights->at(i + 1) * inputs[i];
 		}
 		return MathHelper::Sigmoid(output);
 	}
 
-	vector<double>& NeuronC::CloneWeights() {
-		vector<double> clonedWeights(this->GetWeightSize());
-		for each(double weight in this->weights) {
-			clonedWeights.push_back(weight);
-		}
-		return clonedWeights;
+	shared_ptr<vector<double>> NeuronC::CloneWeights() {
+		return make_shared<vector<double>>(*this->weights);
 	}
 }
--- neural/Neuron.h
+++ neural/Neuron.h
@@ -5,20 +5,20 @@ namespace Neural {
 	using namespace std;
 	class NeuronC {
 	private:
-		vector<double> weights;
+		shared_ptr<vector<double>> weights;
 	public:
-		NeuronC(vector<double>& weights);
+		NeuronC(shared_ptr<vector<double>> weights);
 		double GetWeight(size_t weightIndex);
 		void AddWeight(double weight);
 		void RemoveNeuronWeight(size_t neuronIndex);
-		void SetWeights(vector<double>& weights);
+		void SetWeights(shared_ptr<vector<double>> weights);
 		double GetNeuronWeight(size_t neuronIndex);
 		double GetThreshold();
 		void SetWeight(size_t weightIndex, double weight);
 		void SetThresholdWeight(double weight);
 		void SetNeuronWeight(size_t neuronIndex, double weight);
-		double GetOutput(vector<double>& inputs);
+		double GetOutput(const vector<double>& inputs);
 		size_t GetWeightSize();
-		vector<double>& CloneWeights();
+		shared_ptr<vector<double>> CloneWeights();
 	};
 }
